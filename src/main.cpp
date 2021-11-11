c52859b1f7bc7b05db18f51c1a5b45069f36a1dd
jwhitlow45 <jwhitlow45@gmail.com>
2021-11-10 21:46:39 -0500

    feat:get command line arguments for tp and tc

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
+int getCommandLineArgument(vector<string>, string);
 
-    cout << "Hello world!";
+int main(int argc, char *argv[])
+{
+    //get command line arguments
+    vector<string> args;
+    for (size_t i = 0; i < argc; i++)
+        args.push_back(string(argv[i]));
+    int tpVal = getCommandLineArgument(args, "tp");
+    int tcVal = getCommandLineArgument(args, "tc");
+
+    cout << tpVal << " " << tcVal << endl;
 
     return 0;
+}
+
+int getCommandLineArgument(vector<string> args, string prefix)
+{
+    for (string arg : args)
+    {
+        if (arg.size() > prefix.size() + 2 && prefix == arg.substr(1, prefix.size()))
+        {
+            return stoi(arg.substr(prefix.size() + 2, arg.size() - prefix.size()));
+        }
+    }
+    return -1;
 }
